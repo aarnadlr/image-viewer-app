fab6087173dea1640076ef91e5f3e48c77f8a108
aarnadlr <aaronadler@gmail.com>
2022-03-20 22:11:11 -0400

    Move theme file to fix build.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/global.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from './theme';
+import { theme } from '../utils/theme';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
