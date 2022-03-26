3e8102937770c241e3b3ab0f41e2958d1506bc72
aarnadlr <aaronadler@gmail.com>
2022-03-25 21:29:56 -0400

    Add dark mode toggle.

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,12 @@ import {
   Heading,
   Button,
   useBreakpointValue,
+  useColorMode,
+  useColorModeValue,
+  IconButton,
 } from '@chakra-ui/react';
 import { UploadModal } from '../components/UploadModal';
-import { DownloadIcon } from '@chakra-ui/icons';
+import { DownloadIcon, SunIcon, MoonIcon } from '@chakra-ui/icons';
 import { zipFiles } from '../utils/zipFiles';
 import styled from '@emotion/styled';
 
@@ -34,6 +37,8 @@ export function Header({
   };
 
   const buttonSize = useBreakpointValue({ base: 'sm', md: 'lg' });
+  const { colorMode, toggleColorMode } = useColorMode();
+  const color = useColorModeValue('purple.500', 'purple.200');
 
   return (
     <Nav flexDirection={{ base: 'column', md: 'row' }}>
@@ -42,7 +47,7 @@ export function Header({
           fontSize={'2rem'}
           height={'40px'}
           lineHeight={1.2}
-          color={'purple.500'}
+          color={color}
           marginBottom={'.5rem'}
         >
           Image Viewer App
@@ -52,7 +57,7 @@ export function Header({
           fontSize={'1rem'}
           height={'20px'}
           lineHeight={1.2}
-          color={'purple.500'}
+          color={color}
           fontWeight={500}
         >
           Select images to download
@@ -66,7 +71,7 @@ export function Header({
           onClick={handleZipFiles}
           size={buttonSize}
           colorScheme="gray"
-          margin="1rem 1rem 1rem 0"
+          margin="1rem .5rem 1rem 0"
           disabled={checkedImagesArr.length > 0 ? false : true}
         >
           Download Selected Files
@@ -75,6 +80,14 @@ export function Header({
         <UploadModal
           resourceObjectsArr={resourceObjectsArr}
           loadImages={loadImages}
+        />
+
+        <IconButton
+          margin="0 0 0 .5rem"
+          size={buttonSize}
+          onClick={toggleColorMode}
+          aria-label="color mode"
+          icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
         />
       </Box>
     </Nav>
